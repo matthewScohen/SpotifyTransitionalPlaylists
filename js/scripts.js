efe64da6cc54643f02608276ce77893c025a234a
matthewScohen <cohen.matthew@ufl.edu>
2020-09-15 19:53:29 -0400

    update redirect uri

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 //   'width=800,height=600'
 // )
 
-window.location.replace("https://accounts.spotify.com/authorize?client_id=4f80521dc4c84c6eb92da6f9b5c06458&response_type=code&redirect_uri=https://google.com&scope=playlist-modify-public%20playlist-read-private%20playlist-modify-private&state=34fFs29kd09",)
+window.location.replace("https://accounts.spotify.com/authorize?client_id=4f80521dc4c84c6eb92da6f9b5c06458&response_type=code&redirect_uri=https://matthewscohen.github.io/SpotifyTransitionalPlaylists/&scope=playlist-modify-public%20playlist-read-private%20playlist-modify-private&state=34fFs29kd09",)
 
 window.spotifyCallback = (payload) => {
   popup.close()
