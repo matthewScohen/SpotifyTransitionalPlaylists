d60c6619cc05347fd4736332879297265faccdf6
matthewScohen <cohen.matthew@ufl.edu>
2020-09-15 21:03:02 -0400

    update login

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,1 @@
-window.location.replace("https://accounts.spotify.com/authorize?client_id=4f80521dc4c84c6eb92da6f9b5c06458&response_type=code&redirect_uri=&scope=playlist-modify-public%20playlist-read-private%20playlist-modify-private&state=34fFs29kd09",)
+window.location.replace("https://accounts.spotify.com/authorize?client_id=4f80521dc4c84c6eb92da6f9b5c06458&response_type=code&redirect_uri=https://matthewscohen.github.io/SpotifyTransitionalPlaylists/&scope=playlist-modify-public%20playlist-read-private%20playlist-modify-private&state=34fFs29kd09",)
