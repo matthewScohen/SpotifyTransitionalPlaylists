b707a3a993a06655fa63289040489b0be91c3fb3
matthewScohen <cohen.matthew@ufl.edu>
2020-09-15 21:18:31 -0400

    update token access

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,5 @@ if(window.location.hash == "")
   window.location.replace("https://accounts.spotify.com/authorize?client_id=4f80521dc4c84c6eb92da6f9b5c06458&response_type=token&redirect_uri=https://matthewscohen.github.io/SpotifyTransitionalPlaylists/&scope=playlist-modify-public%20playlist-read-private%20playlist-modify-private&state=34fFs29kd09",)
 }
 token = window.location.hash;
+// token = window.location.hash.substr(1).split('&')[0].split("=")[1]
 console.log(token);
